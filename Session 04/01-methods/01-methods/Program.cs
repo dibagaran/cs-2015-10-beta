2e65fbea4f2dc667c4210d60e46780a4c25bb635
Meysam Mousavi <xeronith@gmail.com>
2015-10-31 22:58:32 +0330

    //

---
+++ Session 04/01-methods/01-methods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string choice = DisplayMenu();
+                Process(choice);
+
+                Console.ReadKey(true);
+            }
+        }
+
+        static string DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. List Items");
+            Console.WriteLine("3. Exit");
+            return Console.ReadLine();
+        }
+
+        static void Process(string choice)
+        {
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Adding a new item ...");
+                    break;
+                case "2":
+                    Console.WriteLine("Listing all items ...");
+                    break;
+                case "3":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Session 04/02-value-types-and-reference-types/02-value-types-and-reference-types/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0; // This is a value type.
+            int[] j = new int[10]; // All arrays are reference types.
+
+            ValueTypeTest(i);
+            Console.WriteLine(i); // The output is 0 because just a copy of value is passed to the method.
+
+            ReferenceTypeTest(j);
+            Console.WriteLine(j[0]); // The output is 1 because a copy of reference is passed to the method.
+
+            ValueTypeByRefTest(ref i);
+            Console.WriteLine(i); // The output is 1 because the value is passed by reference.
+        }
+
+        static void ValueTypeTest(int param)
+        {
+            param++;
+        }
+
+        static void ReferenceTypeTest(int[] param)
+        {
+            param[0]++;
+        }
+
+        static void ValueTypeByRefTest(ref int param)
+        {
+            param++;
+        }
+    }
+}
